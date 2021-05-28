37aafdb81c3d3655bd339e455f17c9205f9f0cc8
GC <gcoria1989@gmail.com>
2021-05-28 08:41:02 -0300

    Agregando punto 1 de la unidad 2

---
+++ 02-JS-I/homework/homework-2-punto-1.js
@@ -0,0 +1,16 @@
+Variables: ahí guardamos información, para poder usarla mas tarde en la computadora. 
+Para usar la variable, le tenemos que poner un nombre que sea único.
+Se le dice "variable" porque la información puede variar. 
+
+Strings: son letras, también pueden ser palabras. 
+Pero, para que sea un "string", les tenemos que poner una comilla al inicio y al final. 
+
+Funciones (argumentos, return): una función es algo que armamos para que haga algo que querramos. 
+Una especie de mini programa.
+Un argumento es lo que queremos que la función haga. Lo escribimos al principio. 
+El "return" es el resultado de lo que hizo la función.
+
+Declaraciones if: nos sirve para ayudar a la máquina a que decida algo. Decide por si o por no.
+
+Valores booleanos (true, false): solo son dos, verdadero o falso. 
+Lo usamos cuando queremos que la máquina haga algo o no, según si algo es verdadero o falso.
