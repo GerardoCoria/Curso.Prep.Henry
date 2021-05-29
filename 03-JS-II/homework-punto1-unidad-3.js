91d2ea3fc1fbf50c032b88ff0cc70774fc0a9251
GC <gcoria1989@gmail.com>
2021-05-29 12:27:50 -0300

    Agregando nuevo archivo unidad 3, homework/homework.js

---
+++ 03-JS-II/homework-punto1-unidad-3.js
@@ -0,0 +1,13 @@
+Bucles for: sirve para decirle a la computadora que haga algo, una y otra vez. 
+Y que se detenga cuando nosotros se lo indiquemos.
+Tenemos que decirle el numero con el que comenzamos. Y tener cuidado en hacer que ese numero vaya sumando de a uno en cada vuelta.
+O sea, va a dar vueltas y vueltas, y en cada vuelta va a aumentar uno.
+
+Operador lógico &&: me sirve para decirle a la máquina que haga o no algo. 
+Lo hago al comparo dos datos. Y solamente se hace lo que programé cuando los dos datos son verdaderas. 
+
+Operador lógico ||: sirve también para decirle a la computadora si hace o no hace algo. 
+Pero con la diferencia de que se hace cuando las dos cosas o una sola es verdadera. 
+
+Operador lógico !: me cambia el significado de lo que tengo al lado. 
+Si es verdadero, lo vuelve falso. O si es falso, lo vuelve verdadero.
