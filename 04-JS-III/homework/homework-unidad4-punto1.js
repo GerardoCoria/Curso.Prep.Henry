fa5ea71e5e6dffa681d59f84c590d9699c890309
GC <gcoria1989@gmail.com>
2021-05-30 12:18:06 -0300

    Agrego archivo de unidad 4, 04-JS-III/homework/homework.js

---
+++ 04-JS-III/homework/homework-unidad4-punto1.js
@@ -0,0 +1,6 @@
+ARRAYS: son parecidos a una lista. La cual podemos modificar.
+Podemos agrgarle o quitarle elementos a esa lista.
+Lo importante es que juntamos varias cosas en un solo lugar.
+Los elementos pueden ser números, letras, formulas.
+Y podemos usar los elementos de la lista de forma individual.
+El array es como si fuera una biblioteca, donde ponemos datos en forma ordenada.
