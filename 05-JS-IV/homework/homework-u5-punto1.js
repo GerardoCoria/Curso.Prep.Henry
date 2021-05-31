f456036847032e3e78219820dec27642bd35df33
GC <gcoria1989@gmail.com>
2021-05-31 09:55:14 -0300

    Agrego homework unidad 5, homework-u5-punto1.js

---
+++ 05-JS-IV/homework/homework-u5-punto1.js
@@ -0,0 +1,19 @@
+Objetos: tienen un conjunto de cualidades, tales como tendría un objeto real.
+Por ej, una mesa tiene cualidades, colores, medidas, peso, etc. 
+Por lo tanto un objeto en JS tiene un aspecto similar.
+Puede haber una fórmula como valor.
+Un objeto de JS tiene cualidades y valores, que lo hacen único.
+
+Propiedades: une a la caracteristica del objeto con el valor correpsondiente. 
+Por ej. la propiedad en una mesa es la relacion entre medidas con el valor (por ej 1, 40 mts.)
+Un objeto se conforma de muchas propiedades.
+
+Métodos: son funciones del JS que permiten hacer alguna operación. 
+Se ubican en los valores.
+
+Bucle for…in: busca dentro del objeto el valor señalado. 
+Da vueltas por todas las caracteristicas del objeto, y realiza lo programado.
+
+Notación de puntos: usamos un punto entre el nombre del objeto y la caracteristica que queremos que aparezca.
+
+Notación de corchetes: tenemos que usar corchetes y comillas para que aparezca la caracteristica de objeto que queremos.
