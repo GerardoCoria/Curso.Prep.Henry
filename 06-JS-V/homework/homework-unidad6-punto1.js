94965fded021d1ce8dedab6b12a8616ee40b8463
GC <gcoria1989@gmail.com>
2021-06-01 17:34:58 -0300

    agrego tarea unidad 6, homework-unidad6-punto1.js

---
+++ 06-JS-V/homework/homework-unidad6-punto1.js
@@ -0,0 +1,7 @@
+Prototype: sirve para armar una vez las formulas, y usarlas cuando las necesitemos.
+Así, no llenamos en exceso la memoria de la computadora. 
+El Prototype es como una plantilla, o molde para los objetos.
+Si el objeto no tiene una caracteristica, se la pide a su Prototype.
+
+Constructors (de Clases): es una funcion, para crear nuevos objetos (o sea un conjunto de caraacteristicas, datos, caualidades)
+a partir de el mismo.
